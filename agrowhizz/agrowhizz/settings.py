8b15f036bc9f3b4dd3e9cb1bed6d0613cbb39a1f
patbpm <patbpm1@gmail.com>
2018-06-13 11:41:47 +0200

    agrowhizzdb mysql setting and creation

--- agrowhizz/agrowhizz/settings.py
+++ agrowhizz/agrowhizz/settings.py
@@ -74,12 +74,23 @@ WSGI_APPLICATION = 'agrowhizz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#   }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'agrowhizzdb',
+        'USER':'root',
+        'PASSWORD':'',
+        'PORT':'',
+        'HOST': 'localhost',
     }
 }
+
 
 
 # Password validation
