a2fc4544dee1241d71030d7e011388e8c6d57f20
patbpm <patbpm1@gmail.com>
2018-06-14 13:01:40 +0200

    Adding post detail html file

--- agrowhizz/blog/views.py
+++ agrowhizz/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
@@ -11,3 +11,11 @@ def blog(request):
         'posts': posts
     }
     return render(request, 'blog/blog.html', context)
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    context = {
+        'post': post
+    }
+    return render(request, 'blog/post_detail.html', context)
