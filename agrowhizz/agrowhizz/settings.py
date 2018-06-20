9fa18a7e324dd5b45b2a0e8886b1472f1ff85f09
patbpm <patbpm1@gmail.com>
2018-06-20 11:49:24 +0200

    setting up python decouple to hide sentive info

--- agrowhizz/agrowhizz/settings.py
+++ agrowhizz/agrowhizz/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from decouple import config, Csv
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +22,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dg%c&ls1%*-6qv3b0xn^i&+*5ilayd14)+v-yu*hq1-$pe5g%@'
+SECRET_KEY = config('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
@@ -94,6 +97,7 @@ DATABASES = {
         'PORT':'',
         'HOST': 'localhost',
     }
+
 }
 
 
