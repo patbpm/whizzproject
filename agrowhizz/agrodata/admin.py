824a52175811b541243802d5f438544f65ab1ce2
patbpm <patbpm1@gmail.com>
2018-08-16 16:28:15 +0200

    Creating the first model for agrodata app

--- agrowhizz/agrodata/admin.py
+++ agrowhizz/agrodata/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Business_stream
+
+admin.site.register(Business_stream)
--- agrowhizz/agrodata/models.py
+++ agrowhizz/agrodata/models.py
@@ -1,3 +1,23 @@
+import math
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils.html import mark_safe
 # Create your models here.
+
+
+class Business_stream(models.Model):
+    business_stream_name = models.CharField(max_length=30, unique=True)
+    business_stream_description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.business_stream_name
+
+class Company(models.Model):
+    company_name = models.CharField(max_length=100, unique=True)
+    profile_description = models.CharField(max_length=100)
+    business_stream_id = models.ForeignKey(Business_stream, related_name='companies',on_delete=models.PROTECT)
+    last_updated = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return self.company_name
