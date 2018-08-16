865a104edda65f2dfeb22016cef6318ee9f57be5
patbpm <patbpm1@gmail.com>
2018-08-16 16:44:46 +0200

    Creating the first view for the first model for agrodata app

--- agrowhizz/agrodata/views.py
+++ agrowhizz/agrodata/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from .models import Business_stream
 # Create your views here.
 def agrodata(request):
     return render(request, 'agrodata/agrodata.html', {})
 
 def foodindustrydata(request):
-    return render(request, 'agrodata/foodindustrydata.html', {})
+    business_streams = Business_stream.objects.all()
+    context = {
+        'business_streams': business_streams,  
+    }
+    return render(request, 'agrodata/foodindustrydata.html', context)
