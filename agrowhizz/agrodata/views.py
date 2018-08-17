e6db2b01c465cfa01ef73c3bc74ad05e1251ef34
patbpm <patbpm1@gmail.com>
2018-08-17 16:53:55 +0200

    fixing agrodata files with the design

--- agrowhizz/agrodata/views.py
+++ agrowhizz/agrodata/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from .models import Database
 # Create your views here.
 def agrodata(request):
+    
+    return render(request, 'agrodata/agrodata.html', {})
+
+def foodindustrydata(request):
     databases = Database.objects.all()
     context = {
         'databases': databases,  
     }
-    return render(request, 'agrodata/agrodata.html', context)
-
-def foodindustrydata(request):
-    
-    return render(request, 'agrodata/foodindustrydata.html', {})
+    return render(request, 'agrodata/foodindustrydata.html', context)
