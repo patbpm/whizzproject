5a552975897396cf5b74c0776dc6962274546a92
patbpm <patbpm1@gmail.com>
2018-08-20 12:58:14 +0200

    Adding login requirement in the agrodat view file

--- agrowhizz/agrodata/views.py
+++ agrowhizz/agrodata/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .models import Database
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def agrodata(request):
     
     return render(request, 'agrodata/agrodata.html', {})
 
+@login_required
 def foodindustrydata(request):
     databases = Database.objects.all()
     context = {
