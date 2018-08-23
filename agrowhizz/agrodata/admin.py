6403b80c45808ea0dfd84b31c77f9538c4aebac1
patbpm <patbpm1@gmail.com>
2018-08-23 14:13:55 +0200

    Fixing te agrodata models

--- agrowhizz/agrodata/admin.py
+++ agrowhizz/agrodata/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Database
+from .models import Database, Company
 
 admin.site.register(Database)
+admin.site.register(Company)
--- agrowhizz/agrodata/models.py
+++ agrowhizz/agrodata/models.py
@@ -15,7 +15,7 @@ class Database(models.Model):
 class Company(models.Model):
     company_name = models.CharField(max_length=100, unique=True)
     profile_description = models.CharField(max_length=100)
-    business_stream_id = models.ForeignKey(Database, related_name='companies',on_delete=models.PROTECT)
+    database = models.ForeignKey(Database, related_name='companies',on_delete=models.PROTECT)
     last_updated = models.DateTimeField(auto_now_add=True)
     
     
