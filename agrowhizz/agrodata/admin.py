dd395f27b7e097f3032734ba719574708c92f0eb
patbpm <patbpm1@gmail.com>
2018-08-30 12:08:48 +0200

    Adding the model for Ingredienmts Categories and ingredients

--- agrowhizz/agrodata/admin.py
+++ agrowhizz/agrodata/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Database, Company, CompanyAddress
+from .models import Database, Company, CompanyAddress, IngredientsCategories, Ingredients
 
 admin.site.register(Database)
 admin.site.register(Company)
 admin.site.register(CompanyAddress)
+admin.site.register(IngredientsCategories)
+admin.site.register(Ingredients)
--- agrowhizz/agrodata/models.py
+++ agrowhizz/agrodata/models.py
@@ -45,3 +45,23 @@ class CompanyAddress(models.Model):
     
     def __str__(self):
         return self.location
+
+# Database for all the Ingredients Categories #
+class IngredientsCategories(models.Model):
+    category = models.CharField(max_length=50, unique=True)
+    
+
+    def __str__(self):
+        return self.category
+
+# Database for all The Ingredients #
+class Ingredients(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.ForeignKey(IngredientsCategories, related_name='Ingredients',on_delete=models.PROTECT)
+    company_name = models.ForeignKey(Company, related_name='Ingredients',on_delete=models.PROTECT)
+    product_code = models.CharField(max_length=10)
+    last_updated = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return self.name
