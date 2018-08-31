8079d5e69f58b443de79888bddcbf25507f85861
patbpm <patbpm1@gmail.com>
2018-08-31 16:36:30 +0200

    Making a Food Categories and Ingredient list files

--- agrowhizz/agrodata/urls.py
+++ agrowhizz/agrodata/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.agrodata, name='agrodata'),
-    url(r'^foodindustrydata/$', views.foodindustrydata, name='foodindustrydata'),
+    url(r'^foodindustrydata/$', views.CompanyCategoryListView.as_view(), name='foodindustrydata'),
+    url(r'^foodcategoriesdata/$', views.FoodCategoryListView.as_view(), name='foodcategoriesdata'),
     url(r'^foodindustrydata/(?P<pk>\d+)/$', views.companyList, name='companyList'),
+    url(r'^foodcategoriesdata/(?P<pk>\d+)/$', views.ingredientList, name='ingredientList'),
     url(r'^foodindustrydata/(?P<pk>\d+)/companydetails/(?P<company_pk>\d+)/$', views.companyDetails, name='companyDetails'),
 ]
--- agrowhizz/agrodata/views.py
+++ agrowhizz/agrodata/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+from django.utils.decorators import method_decorator
 from .models import Database, Company, CompanyAddress, IngredientsCategories, Ingredients
 from django.contrib.auth.decorators import login_required
 # Create your views here.
@@ -6,16 +8,24 @@ def agrodata(request):
     
     return render(request, 'agrodata/agrodata.html', {})
 
-@login_required
-# view for all the food classification #
-def foodindustrydata(request):
-    databases = Database.objects.all().order_by('name')
-    context = {
-        'databases': databases,  
-    }
-    return render(request, 'agrodata/foodindustrydata.html', context)
 
-# View for all the Comany Name #
+# view for all the Company classification #
+@method_decorator(login_required, name='dispatch')
+class CompanyCategoryListView(ListView):
+    model = Database
+    context_object_name = 'databases'
+    template_name = 'agrodata/foodindustrydata.html'
+    ordering = 'name'
+
+# view for all the food classification #    
+@method_decorator(login_required, name='dispatch')
+class FoodCategoryListView(ListView):
+    model = IngredientsCategories
+    context_object_name = 'foodCategories'
+    template_name = 'agrodata/foodcategoriesdata.html'
+    
+
+# View for all the Company Name #
 def companyList(request, pk):
     databases = Database.objects.get(pk=pk)
     companies = databases.companies.order_by('company_name')
@@ -27,16 +37,32 @@ def companyList(request, pk):
     }
     return render(request, 'agrodata/companyList.html', context)
 
+# View for all the Company Name #
+def ingredientList(request, pk):
+    categories = IngredientsCategories.objects.get(pk=pk)
+    ingredients = categories.Ingredients.order_by('name')
+    
+    context = {
+        'categories': categories,
+        'ingredients': ingredients,
+        
+    }
+    return render(request, 'agrodata/ingredientList.html', context)
+
 def companyDetails(request, pk, company_pk):
     
     company = get_object_or_404(Company, database__pk=pk, pk=company_pk)
     addresses = company.companyAddress.get()
-    categories = IngredientsCategories.objects.all()
+    categories = company.Ingredients.all()
+    ingredients = company.Ingredients.all()
+
+
+
     context = {
        
        'company': company,
        'addresses': addresses,
        'categories': categories,
-        
+       'ingredients': ingredients,
     }
     return render(request, 'agrodata/companyDetails.html', context)
